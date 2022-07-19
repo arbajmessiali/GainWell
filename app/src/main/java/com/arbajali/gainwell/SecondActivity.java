4ea84320e6b6d1e047a94d8f541806361e63b978
Intaj Ali <intajcse@gmail.com>
2022-07-19 16:50:46 +0530

    implement WebView in second activity

--- app/src/main/java/com/arbajali/gainwell/SecondActivity.java
+++ app/src/main/java/com/arbajali/gainwell/SecondActivity.java
@@ -3,12 +3,38 @@ package com.arbajali.gainwell;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        webview = new WebView(this);
+        webview.loadUrl("https://gainwellsports.com/");
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        this.setContentView(webview);
+    }
+
+    @Override
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
